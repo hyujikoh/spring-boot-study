88573db72016cb536e2fa969574a1cda65fe7fd0
hyujikoh <42793489+hyujikoh@users.noreply.github.com>
2022-01-13 23:32:27 +0900

    Update spring_boot.java

--- spring-boot/src/spring_boot.java
+++ spring-boot/src/spring_boot.java
@@ -4,9 +4,10 @@ class Solution {
     public void solution(int w, int h) {
         long answer = 0;
         long gcd = 0;
-        long width = Long.valueOf(w);
-        long height =Long.valueOf(h);
+        long width = Long.valueOf(w); // int 형 자료 long 전환
+        long height =Long.valueOf(h); // int 형 자료 long 전환
         if(w>h){
+	    // 멀쩡한 삼각형 구하는 공식 w와 h의 대소 관계에 따라 조건 실행
             answer = width*height-(width+height-gcd(width,height));
         }
         else{
@@ -14,6 +15,7 @@ class Solution {
         }
         System.out.println(answer);;
     }
+    // 최소공약수 구하는 메소드
     static long gcd(long a, long b){
 
         if(a%b==0){
